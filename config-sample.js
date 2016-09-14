50e051447915648ddeb9982cc362a868bc828551
mikefab <mikefabrikant@gmail.com>
2016-09-14 16:19:36 -0400

    Add wait_time to config

--- config-sample.js
+++ config-sample.js
@@ -1,9 +1,11 @@
 module.exports = {
-  db_name: '',
-  collection_name: '',
+  wait_time: 700,
+  uri: 'https://xxx.documents.azure.com:443/',
+  db_name: 'xxxx',
+  collection_name: 'xxx',
   host: 'https://xxxx.documents.azure.com:443/',
-  primary_key: 'xxxxxx==',
-  secondary_key: 'xxxxx==',
-  primary_connection_string: 'AccountEndpoint=https://xxxxx.documents.azure.com:443/;AccountKey=xxxx==;',
-  secondary_connection_string: 'AccountEndpoint=https://xxxx.documents.azure.com:443/;AccountKey=xxx==;'
+  primary_key: 'xxxx==',
+  secondary_key: 'xxxx==',
+  primary_connection_string: 'AccountEndpoint=https://xxx.documents.azure.com:443/;AccountKey=xxxx==;',
+  secondary_connection_string: 'AccountEndpoint=https://xxxx.documents.azure.com:443/;AccountKey=xxxx==;'
 };
